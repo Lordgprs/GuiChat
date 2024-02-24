fe942ddb4b970b852193e14a6c2c5c1bc430b8e8
Maksim Velgach <m.velgach@inbox.ru>
2024-02-24 23:34:57 +0300

    Finished message sending feature

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,11 +2,18 @@
 #include "Database.h"
 #include "startscreen.h"
 #include "ui_mainwindow.h"
+#include <QDialog>
+#include <QDialogButtonBox>
+#include <QListWidget>
+#include <QTimer>
+#include <QVBoxLayout>
 
 size_t MainWindow::kInstanceCount = 0;
 
-MainWindow::MainWindow(std::shared_ptr<Database> dbPtr, QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow) {
+MainWindow::MainWindow(size_t userId, QString userName,
+                       std::shared_ptr<Database> dbPtr, QWidget *parent)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow), m_userId{userId}, m_userName{userName} {
   ui->setupUi(this);
   ++kInstanceCount;
   if (dbPtr) {
@@ -14,6 +21,9 @@ MainWindow::MainWindow(std::shared_ptr<Database> dbPtr, QWidget *parent)
   } else {
     m_dbPtr = std::make_shared<Database>();
   }
+  auto timer{new QTimer{this}};
+  connect(timer, &QTimer::timeout, this, &MainWindow::updateChats);
+  timer->start(MainWindow::TIMEOUT);
 }
 
 MainWindow::~MainWindow() {
@@ -29,7 +39,7 @@ MainWindow *MainWindow::createClient(std::shared_ptr<Database> dbPtr) {
   if (result == QDialog::Rejected) {
     return nullptr;
   }
-  auto w{new MainWindow(dbPtr)};
+  auto w{new MainWindow(s.userId(), s.userName(), dbPtr)};
   w->setAttribute(Qt::WA_DeleteOnClose);
   return w;
 }
@@ -38,17 +48,77 @@ void MainWindow::on_messageLineEdit_returnPressed() {
   on_sendMessageButton_clicked();
 }
 
-void MainWindow::on_sendMessageButton_clicked() {}
+void MainWindow::on_sendMessageButton_clicked() {
+  m_dbPtr->addChatMessage(m_userName.toStdString(),
+                          ui->messageLineEdit->text().toStdString());
+}
 
-void MainWindow::on_privateMessageSendButton_clicked() {}
+void MainWindow::on_privateMessageSendButton_clicked() {
+  QDialog dial{this};
+  dial.setModal(true);
+  auto *l{new QVBoxLayout};
+  dial.setLayout(l);
+  auto userListWgt{new QListWidget{&dial}};
+  l->addWidget(userListWgt);
+  auto buttonBox{
+      new QDialogButtonBox{QDialogButtonBox::Ok | QDialogButtonBox::Cancel}};
+  l->addWidget(buttonBox);
 
-void MainWindow::on_actionOpen_another_client_triggered() {
+  connect(buttonBox, &QDialogButtonBox::accepted, &dial, &QDialog::accept);
+  connect(buttonBox, &QDialogButtonBox::rejected, &dial, &QDialog::reject);
+
+  auto userList{m_dbPtr->getUserList()};
+  for (const auto &user : userList) {
+    userListWgt->addItem(QString::fromStdString(user));
+  }
+  userListWgt->setCurrentRow(0);
+
+  auto result{dial.exec()};
+  if (result == QDialog::Accepted && userListWgt->currentItem()) {
+    m_dbPtr->addPrivateMessage(m_userName.toStdString(),
+                               userListWgt->currentItem()->text().toStdString(),
+                               ui->messageLineEdit->text().toStdString());
+  }
+}
+
+void MainWindow::updateChats() {
+  auto chatMessages = m_dbPtr->getChatMessages();
+  QString chat;
+  for (const auto &msg : chatMessages) {
+    chat.append(QString::fromStdString(msg) + '\n');
+  }
+  ui->commonChatBrowser->setText(chat);
+
+  chat.clear();
+  auto privateMessages = m_dbPtr->getPrivateMessage(m_userId);
+  for (const auto &msg : privateMessages) {
+    if (msg.getSender() != m_userName.toStdString() &&
+        msg.getDest() != m_userId) {
+      continue;
+    }
+    QString prefix;
+    if (m_userName.toStdString() == msg.getSender() &&
+        m_userId == msg.getDest()) {
+      prefix = tr("self") + ": ";
+    } else if (m_userName.toStdString() == msg.getSender()) {
+      prefix = tr("you to") + ": " +
+               QString(" <%1>: ").arg(
+                   QString::fromStdString(m_dbPtr->getUserName(msg.getDest())));
+    } else {
+      prefix = QString("<%1>").arg(QString::fromStdString(
+                   m_dbPtr->getUserName(msg.getDest()))) +
+               tr(" to you: ");
+    }
+    chat.append(prefix + QString::fromStdString(msg.getText() + '\n'));
+  }
+  ui->privateChatBrowser->setText(chat);
+}
+
+void MainWindow::on_actionNew_instance_triggered() {
   auto w{createClient(m_dbPtr)};
   if (w) {
     w->show();
   }
 }
 
-void MainWindow::on_actionClose_this_client_triggered() {
-  QApplication::exit(EXIT_SUCCESS);
-}
+void MainWindow::on_actionClose_triggered() { this->close(); }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,7 @@
 
 #include "Database.h"
 #include <QMainWindow>
+#include <chrono>
 #include <memory>
 
 namespace Ui {
@@ -13,23 +14,27 @@ class MainWindow : public QMainWindow {
   Q_OBJECT
 
 public:
-  explicit MainWindow(std::shared_ptr<Database> dbPtr = nullptr,
+  explicit MainWindow(size_t userId, QString userName,
+                      std::shared_ptr<Database> dbPtr = nullptr,
                       QWidget *parent = nullptr);
   ~MainWindow();
   static MainWindow *createClient(std::shared_ptr<Database> dbPtr = nullptr);
   static size_t kInstanceCount;
 
 private slots:
-
   void on_messageLineEdit_returnPressed();
   void on_sendMessageButton_clicked();
   void on_privateMessageSendButton_clicked();
-  void on_actionOpen_another_client_triggered();
-  void on_actionClose_this_client_triggered();
+  void updateChats();
+  void on_actionNew_instance_triggered();
+  void on_actionClose_triggered();
 
 private:
   Ui::MainWindow *ui;
   std::shared_ptr<Database> m_dbPtr;
+  size_t m_userId;
+  QString m_userName;
+  const std::chrono::milliseconds TIMEOUT{50ms};
 };
 
 #endif // MAINWINDOW_H
